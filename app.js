ab334e52105ceff740a0eca07f91613aee3d9c6e
John Doe <johndoe@example.com>
2024-01-28 18:51:18 +0100

    update

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 let password = document.getElementById("password");
 
 function generateRandomPassword() {
-    password.innerHTML = "k2Ydn125s"
+    
+    let letterArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","w","x","y","z"];
+    let characterArray = ["!","@","#","$","%","/","&","*","?"];
+    
+    // Characters 1 2 3
+    let randomLetter = Math.floor(Math.random() * 27);
+    let randomNumber = Math.floor(Math.random() * 10);
+    let randomCharacter = Math.floor(Math.random() * 8);
+    
+    // Characters 4 5 6
+    let randomLetter2 = Math.floor(Math.random() * 27);
+    let randomNumber2 = Math.floor(Math.random() * 10);
+    let randomCharacter2 = Math.floor(Math.random() * 8);
+
+    // Characters 7 8 9
+    let randomLetter3 = Math.floor(Math.random() * 27);
+    let randomNumber3 = Math.floor(Math.random() * 10);
+    let randomCharacter3 = Math.floor(Math.random() * 8);
+    
+    password.innerHTML = letterArray[randomLetter] + randomNumber + characterArray[randomCharacter]
+                        + letterArray[randomLetter2] + randomNumber2 + characterArray[randomCharacter2]
+                        + letterArray[randomLetter3] + randomNumber3 + characterArray[randomCharacter3];
+
 }
 generateRandomPassword();
